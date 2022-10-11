462840ea2283cb438db9fb86a229a24c2c3467fd
fatih4242 <fatih16@live.nl>
2022-10-11 16:42:11 +0300

    Initial Commit

---
+++ Joke/JokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeApp.swift
+//  Joke
+//
+//  Created by Fatih Toker on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct JokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
